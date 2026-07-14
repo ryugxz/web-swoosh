7292bf5f06794ac7213c7f45be1d46085231b26c
DevRyu <162642440+ryugxz@users.noreply.github.com>
2026-07-15 03:45:04 +0700

    update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -18,10 +18,6 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-console.log("ENV:", import.meta.env);
-
-console.log("Firebase Config", firebaseConfig);
-
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
